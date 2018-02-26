2f39064096f51d8b9bc85d0c97b04f7dbba31d49
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 18:58:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,14 @@ class Student
     @grade = grade
   end
 
-  def self.create_table(name:, grade:)
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        )
+        SQL
   end
 
   def self.drop_table
