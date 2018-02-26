700457945c25c2f8da84487d286915983612c24e
Berry  Semexan <bsemexan@gmail.com>
2018-02-26 18:52:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,11 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
+  def initialize(id:, name:, grade:)
+    @id = id 
+    @name = name 
+    @grade = grade
+  end
   def self.create_table(name:, grade:)
 
 end
